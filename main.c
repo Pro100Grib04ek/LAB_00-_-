c7afabbb409411d2f48fc757be52616aece937fb
Pro100Grib04ek <mamnevilia@mail.ru>
2025-03-17 05:27:42 +0300

    Summary (req)
    .

--- main.c
+++ main.c
@@ -1,238 +1,221 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <time.h>
 #include <locale.h>
-#include <malloc.h>
-
 #pragma warning(disable:4996)
+#pragma pack(push, 1) // Выравнивание = 1 байт
 
-extern const char* first_names_f[]; //массивы для случайных имен, фамилий и отчеств (женские)
-extern const int num_first_names_f;
-extern const char* last_names_f[];
-extern const int num_last_names_f;
-extern const char* middle_names_f[];
-extern const int num_middle_names_f;
-extern const char* first_names_m[]; //массивы для случайных имен, фамилий и отчеств (мужские)
-extern const int num_first_names_m;
-extern const char* last_names_m[];
-extern const int num_last_names_m;
-extern const char* middle_names_m[];
-extern const int num_middle_names_m;
-extern const char* subjects[]; //массив для учебных предметов
-extern const int num_subjects;
-extern const char* group[];
-extern const int num_group; //массив для номера группы
-extern const char* subject_format[];
-
-//количество элементов в массивах
-#define NUM_SUBJECTS 12
-
-//максимальная длина строки
-#define MAX_NAME_LENGTH 100
-
-//структура для дисциплин
-typedef struct
-{
-    char name[43]; // Самое длинное название - 42 символа
-    char room_lec[4]; // < 256
-    char room_lab[4]; // < 256
-    char hours[4]; // < 256
-    char format[10]; // Экзамен, зачёт, курсовая
-} Subject;
-
-//структура для хранения данных
+/* ========== Структуры данных ========== */
 typedef struct {
-    char gender;
-    char* first_name;
-    char* middle_name;
-    char* last_name;
-    int birth_date; // Формат DD.MM.YYYY -> UNIX TIME
-    short num_subjects:5;
-    Subject* subjects;
-    char group[MAX_NAME_LENGTH];
-} Person;
+    const char* name;
+    const char* room_lec;
+    const char* room_lab;
+    uint16_t hours;
+    uint8_t format; // 0-экзамен, 1-зачет, 2-курсовая
+} PackedSubject;
 
-#define IsLeapYear(year) ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) 
+typedef struct {
+    uint8_t num_subjects;
+    PackedSubject* subjects;
+} StudentGroup;
 
-int getTotalDaysInMonth(int month, int year)
-{
-    int days[] = {
-        31, // Jan
-        28, // Feb
-        31, // Mar
-        30, // Apr
-        31, // May
-        30, // Jun
-        31, // Jul
-        31, // Aug
-        30, // Sep
-        31, // Oct
-        30, // Nov
-        31  // Dec
-    };
-    return (days[month - 1] + (IsLeapYear(year) && month == 2 ? 1 : 0));
-}
+typedef struct {
+    uint16_t name_indexes : 15; // 5 бит на каждый индекс (0-26)
+    uint8_t gender : 1;  // 0-муж, 1-жен
+    uint8_t group_index : 7;  // 0-127 групп
+    uint16_t birth_days;  // дни с 01.01.1990 (0-16383)
+    uint8_t flags;  // биты 0-4: кол-во предметов
+} Student;
 
-void UnixToTime(int x) {
-    int year = 1970;
-    int day = 1;
-    int month = 1;
+#pragma pack(pop) // Вернули предыдущую настройку выравнивания
 
-    while (x >= 86400) {
-        x -= 86400;
-        day++;
-        if (day > getTotalDaysInMonth(month, year)) {
-            day = 1;
-            month++;
-            if (month > 12) {
-                year++;
-                month = 1;
-            }
+/* ========== Глобальные константы ========== */
+const char* first_names_f[] = {
+    "Анна", "Мария", "Елена", "Ольга", "Татьяна", "Наталья", "Ирина",
+    "Светлана", "Екатерина", "Александра", "Людмила", "Вера", "Галина",
+    "Лариса", "Валентина", "Нина", "Зинаида", "Маргарита", "Раиса", "Лидия"
+};
+
+const char* first_names_m[] = {
+    "Александр", "Дмитрий", "Михаил", "Сергей", "Андрей", "Иван", "Владимир",
+    "Николай", "Алексей", "Евгений", "Павел", "Константин", "Артем", "Игорь",
+    "Виталий", "Олег", "Юрий", "Василий", "Геннадий", "Станислав"
+};
+
+const char* last_names_f[] = {
+    "Иванова", "Петрова", "Сидорова", "Смирнова", "Кузнецова", "Васильева",
+    "Попова", "Новикова", "Морозова", "Волкова", "Алексеева", "Лебедева",
+    "Семенова", "Егорова", "Павлова", "Козлова", "Степанова", "Николаева",
+    "Орлова", "Андреева"
+};
+
+const char* last_names_m[] = {
+    "Иванов", "Петров", "Сидоров", "Смирнов", "Кузнецов", "Васильев",
+    "Попов", "Новиков", "Морозов", "Волков", "Алексеев", "Лебедев",
+    "Семенов", "Егоров", "Павлов", "Козлов", "Степанов", "Николаев",
+    "Орлов", "Андреев"
+};
+
+const char* middle_names_f[] = {
+    "Александровна", "Андреевна", "Борисовна", "Владимировна", "Дмитриевна",
+    "Евгеньевна", "Ивановна", "Константиновна", "Михайловна", "Николаевна",
+    "Павловна", "Сергеевна", "Федоровна", "Юрьевна", "Валентиновна",
+    "Викторовна", "Геннадьевна", "Леонидовна", "Олеговна", "Станиславовна"
+};
+
+const char* middle_names_m[] = {
+    "Александрович", "Дмитриевич", "Михайлович", "Сергеевич", "Андреевич",
+    "Иванович", "Владимирович", "Николаевич", "Алексеевич", "Евгеньевич",
+    "Павлович", "Константинович", "Артемович", "Игоревич", "Витальевич",
+    "Олегович", "Юрьевич", "Васильевич", "Геннадьевич", "Станиславович"
+};
+
+const char* groups[] = {
+    "5151004/40001", "51510001/40001", "5151001/40002", "5151001/40003",
+    "5151003/40001", "5151003/40002", "5151003/40003", "5151004/40002"
+};
+
+const char* att_format[] = {
+    "Экзамен", "Зачёт", "Курсовая"
+};
+
+PackedSubject all_subjects[] = {
+    {"Высшая математика", "301", "401", 120, 0},
+    {"Физика", "202", "302", 90, 0},
+    {"Физкультура", "Политехник", "Политехник", 60, 1},
+    {"Информационная безопасность", "501", "502", 75, 2},
+    {"История России", "105", "-", 60, 1},
+    {"Философия", "210", "-", 45, 1},
+    {"Структуры данных", "410", "411", 110, 0},
+    {"Английский язык", "508", "509", 80, 1},
+    {"ВВПД", "307", "308", 70, 2},
+    {"Цифровой практикум", "415", "416", 90, 2},
+    {"Алгоритмизация", "412", "-", 100, 0},
+    {"Матанализ", "201", "-", 120, 0},
+    {"Линейная алгебра", "203", "-", 80, 0},
+    {"Дискретная математика", "205", "-", 70, 0},
+    {"Теормех", "305", "306", 85, 0},
+    {"Численные методы", "405", "-", 95, 0},
+    {"Базы данных", "409", "410", 110, 2},
+    {"Проектная деятельность", "100", "-", 60, 2},
+    {"Теория вероятностей", "207", "-", 75, 0},
+    {"Оптимизация", "209", "-", 65, 0},
+    {"ООП", "415", "416", 105, 2},
+    {"Архитектура ПК", "510", "-", 80, 0},
+    {"Микроконтроллеры", "512", "513", 90, 2}
+};
+
+/* ========== Глобальные настройки ========== */
+#define MAX_GROUPS 128
+#define DAYS_OFFSET 631152000 // UNIX время для 01.01.1990
+
+StudentGroup group_cache[MAX_GROUPS];
+uint8_t total_groups = 8;
+
+/* ========== Функции работы с системой ========== */
+void init_system() {
+    for (int i = 0; i < total_groups; i++) {
+        group_cache[i].num_subjects = 12;
+        group_cache[i].subjects = malloc(12 * sizeof(PackedSubject));
+        for (int j = 0; j < 12; j++) {
+            group_cache[i].subjects[j] = all_subjects[rand() % 23];
         }
     }
-    printf("%d-%d-%d\n", day, month, year);
 }
 
-//функция для заполнения структуры случайными данными
-void generate_person(Person* person, int min_disciplines, int max_disciplines)
-{
-    //случайный выбор пола
-    person->gender = (rand() % 2) ? 'M' : 'F'; // 'M' - мужской, 'F' - женский
-    char full_name[MAX_NAME_LENGTH];
-
-    //выбор случайного имени, фамилии и отчества в зависимости от пола
-    if (person->gender == 'F')
-    {
-        person->last_name = last_names_f[rand() % num_last_names_f];
-        person->first_name = first_names_f[rand() % num_first_names_f];
-        person->middle_name = middle_names_f[rand() % num_middle_names_f];
-    }
-    else
-    {
-        person->last_name = last_names_m[rand() % num_last_names_m];
-        person->first_name = first_names_m[rand() % num_first_names_m];
-        person->middle_name = middle_names_m[rand() % num_middle_names_m];
-    }
-
-    //генерация случайной даты рождения
-    // Даты рождения: 946674000 - 1230757199
-    int min_date = 946674000;
-    int max_date = 1230757199;
-    person->birth_date = (rand() % (max_date + 1 - min_date)) + min_date;
-
-    //выбор случайной группы
-    strncpy(person->group, group[rand() % num_group], MAX_NAME_LENGTH - 1);
-
-    //переменная для >_<
-    int num_sub = rand() % num_subjects;
-
-    //выбор случайных учебных предметов
-    int rand_num_subjects = rand() % (max_disciplines + 1 - min_disciplines) + min_disciplines; // Случайное число в диапазоне [min_disciplines, max_disciplines]
-    person->num_subjects = rand_num_subjects;
-
-    //выбор случайных учебных предметов с привязкой
-    for (int i = 0; i < rand_num_subjects; i++)
-    {
-        strncpy(person->subjects[i].name, subjects[rand() % num_subjects], MAX_NAME_LENGTH - 1);
-        sprintf(person->subjects[i].room_lec, "%d", (rand() % 256));
-        sprintf(person->subjects[i].room_lab, "%d", (rand() % 256));
-        sprintf(person->subjects[i].hours, "%d", (rand() % 256));
-        strncpy(person->subjects[i].format, subject_format[rand() % 3], 11);
-    }
+void unix_to_date(uint16_t days) {
+    time_t rawtime = DAYS_OFFSET + days * 86400;
+    struct tm* timeinfo = localtime(&rawtime);
+    printf("%02d.%02d.%d", timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);
 }
 
-void print_students(Person* people, int num_people)
-{
-    for (int i = 0; i < num_people; i++)
-    {
-        printf("Человек %d:\n", i + 1);
-        printf("  Имя: %s %s %s\n", people[i].first_name, people[i].middle_name, people[i].last_name);
-        printf("  Дата рождения: "); UnixToTime(people[i].birth_date);
-        printf("  Группа: %s\n", people[i].group);
-        printf("  Учебные предметы:\n");
-        for (int j = 0; j < people[i].num_subjects; j++)
-        {
-            printf("    - %s\n", people[i].subjects[j]);
-            printf("      Количество часов: %s\n", people[i].subjects[j].hours);
-            printf("      Номер кабинета (лекции): %s\n", people[i].subjects[j].room_lec);
-            printf("      Номер кабинета (лабы): %s\n", people[i].subjects[j].room_lab);
-            printf("      Формат аттестации: %s\n", people[i].subjects[j].format);
-        }
-        printf("\n");
-    }
+/* ========== Основные функции ========== */
+void generate_student(Student* s) {
+    s->gender = rand() % 2;
+    s->name_indexes = (rand() % 20) << 10 | (rand() % 20) << 5 | (rand() % 20);
+    s->group_index = rand() % total_groups;
+    s->birth_days = (rand() % 6200) + 1;
+
+    StudentGroup* g = &group_cache[s->group_index];
+    s->flags = g->num_subjects;
 }
 
-int main()
-{
+const char* get_name(Student* s, int part) {
+    uint8_t idx = (s->name_indexes >> (10 - part * 5)) & 0x1F;
+    return s->gender ?
+        (part == 0 ? last_names_f[idx] :
+            part == 1 ? first_names_f[idx] : middle_names_f[idx]) :
+        (part == 0 ? last_names_m[idx] :
+            part == 1 ? first_names_m[idx] : middle_names_m[idx]);
+}
+
+void print_student(Student* s) {
+    printf("Студент: %s %s %s\n",
+        get_name(s, 0), get_name(s, 1), get_name(s, 2));
+    printf("Группа: %s\n", groups[s->group_index]);
+    printf("Дата рождения: ");
+    unix_to_date(s->birth_days);
+    printf("\nДисциплины:\n");
+
+    StudentGroup* g = &group_cache[s->group_index];
+    for (int i = 0; i < g->num_subjects; i++) {
+        printf("  %s:\n", g->subjects[i].name);
+        printf("    -Количество часов: %hu\n", g->subjects[i].hours);
+        printf("    -Кабинет лекции: %s\n", g->subjects[i].room_lec);
+        printf("    -Кабинет лабы: %s\n", g->subjects[i].room_lab);
+        printf("    -Формат аттестации: %s\n", att_format[g->subjects[i].format]);
+    }
+    printf("----------------\n");
+}
+
+int main() {
     srand(time(NULL));
     setlocale(LC_ALL, "Rus");
+    init_system();
 
-    char request[16]; // Строка с запросом, 16 - Макс. длина запроса
+    Student* students = NULL;
+    int count = 0;
+    long long total_mem = 0;
 
-    Person* people = (Person*)malloc(0); // Далее память будет выделяться realloc внутри процедуры
-    long long mem = 0; // Тут хранится занимаемая память
-    int min_disciplines = 10;
-    int max_disciplines = 20;
-    while (1)
-    {
-        scanf("%s", &request);
+    while (1) {
+        char cmd[16];
+        printf("Введите команду: ");
+        scanf("%s", cmd);
 
-        if (!strcmp(request, "gen")) // Отрицание, т.к. strcmp возвращает 0, при совпадениии строк
-        {
-            int gen_num; // Кол-во для генерации
-            scanf("%d", &gen_num);
-            people = realloc(people, gen_num * sizeof(Person));
-            if (people == NULL)
-            {
-                printf("Недостаточно памяти!\n");
-                continue;
+        if (strcmp(cmd, "gen") == 0) {
+            int n;
+            scanf("%d", &n);
+
+            students = realloc(students, (count + n) * sizeof(Student));
+            for (int i = 0; i < n; i++) {
+                generate_student(&students[count + i]);
             }
-            mem += gen_num * sizeof(Person);
-            for (int i = 0; i < gen_num; i++)
-            {
-                generate_person(&people[i], min_disciplines, max_disciplines);
+
+            total_mem += n * sizeof(Student);
+            count += n;
+        }
+        else if (strcmp(cmd, "print") == 0) {
+            for (int i = 0; i < count; i++)
+                print_student(&students[i]);
+        }
+        else if (strcmp(cmd, "mem") == 0) {
+            printf("Всего студентов: %d\n", count);
+            printf("Память структур: %d b\n\t\t %d kb\n\t\t %d mb\n", total_mem, total_mem/1024, total_mem/1024/1024);
+        }
+        else if (strcmp(cmd, "clear") == 0) {
+            free(students);
+            students = NULL;
+            total_mem = 0;
+            count = 0;
+            /*
+            for (int i = 0; i < total_groups; i++) {
+                free(group_cache[i].subjects);
             }
-        }
-        if (!strcmp(request, "get_size"))
-        {
-            printf("%lli B\n", mem);
-            printf("%3.2Lf Kb\n", (double)mem / 1024);
-            printf("%3.2Lf Mb\n", (double)mem / 1024 / 1024);
-            printf("%3.2Lf Gb\n", (double)mem / 1024 / 1024 / 1024);
-        }
-        if (!strcmp(request, "print_students"))
-        {
-            int print_num; // Сколько стундентов вывести
-            scanf("%d", &print_num);
-            int num_generated_students = mem / sizeof(people[0]);
-            if ((print_num > num_generated_students) || (print_num == -1))
-            {
-                print_num = num_generated_students;
-            }
-            print_students(people, print_num);
-        }
-        if (!strcmp(request, "clean"))
-        {
-            free(people);
-            mem = 0;
-            Person* people = (Person*)malloc(0); // Чтобы не крашилось при попытке геерации после очистки
-        }
-        if (!strcmp(request, "min"))
-        {
-            scanf("%d", &min_disciplines);
-            if (min_disciplines > num_subjects)
-            {
-                min_disciplines = num_subjects;
-            }
-        }
-        if (!strcmp(request, "max"))
-        {
-            scanf("%d", &max_disciplines);
-            if (max_disciplines > num_subjects)
-            {
-                max_disciplines = num_subjects;
-            }
+            */
         }
     }
+
+    return 0;
 }
